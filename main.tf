b0951bddaecccc3001fad3c28b3811b6b6d035b1
Marcos Leal <marcosleal@gmail.com>
2023-10-01 00:20:45 -0400

    refactor s3 bucket into the new terrahouse_aws module (#14)

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
+terraform {
+  cloud {
+    organization = "Bellacollezione"
 
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-    Environment = "Dev"
+    workspaces {
+      name = "terra-house-1"
+    }
   }
 }
 
+module "terraform_aws" {
+  source      = "./modules/terraform_aws"
+  user_uuid   = var.user_uuid
+  bucket_name = var.bucket_name
+}
