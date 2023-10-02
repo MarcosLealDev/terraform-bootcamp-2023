4a08f9715f13a96f81de81b0187e359b77381a87
Marcos Leal <marcosleal@gmail.com>
2023-10-01 21:17:54 -0400

    enabled static website hosting (#16)

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value = module.terraform_aws.bucket_name
+  value       = module.terraform_aws.bucket_name
+}
+
+output "s3_website_endpoint" {
+  description = "S3 Static Webisite Hosting endpoint"
+  value       = module.terraform_aws.website_endpoint
 }
