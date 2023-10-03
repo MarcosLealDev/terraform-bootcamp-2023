35dd50387c091af8add5e5de4ce8c98ed5f17af0
MarcosLealDev <marcosleal@gmail.com>
2023-10-03 01:26:15 -0400

    Invalidate cloudfront stack when content version is changed

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "s3_website_endpoint" {
   description = "S3 Static Webisite Hosting endpoint"
   value       = module.terraform_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description = "The Cloudfront Distribution Domain"
+  value = module.terraform_aws.cloudfront_url
+}
